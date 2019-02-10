5d01b2e9406f2a0b40d1a9d8255ba3292e477e67
yumiduka <yumiduka@outlook.com>
2019-02-10 10:54:43 +0900

    first commit

---
+++ Manage-Password.ps1
@@ -0,0 +1,165 @@
+﻿[CmdletBinding()]
+param(
+  [Parameter(Mandatory, ParameterSetName='List')][Alias('L')][switch]$List,
+  [Parameter(Mandatory, ParameterSetName='Get')][Alias('G')][switch]$Get,
+  [Parameter(Mandatory, ParameterSetName='Set')][Alias('S')][switch]$Set,
+  [Parameter(Mandatory, ParameterSetName='Delete')][Alias('D')][switch]$Delete,
+  [Parameter(Mandatory, ParameterSetName='Import')][Alias('I')][switch]$Import,
+  [Parameter(Mandatory, ParameterSetName='Export')][Alias('E')][switch]$Export,
+  [Parameter(Mandatory, ParameterSetName='Get')][Parameter(Mandatory, ParameterSetName='Set')][Parameter(Mandatory, ParameterSetName='Delete')][Alias('e','env')][string]$Environment,
+  [Parameter(Mandatory, ParameterSetName='Get')][Parameter(Mandatory, ParameterSetName='Set')][Parameter(Mandatory, ParameterSetName='Delete')][Alias('a')][string]$Account,
+  [Parameter(Mandatory, ParameterSetName='Set')][Alias('p')][string]$Password,
+  [string]$Path = '~\.idlist',
+  [Parameter(ParameterSetName='Import')][Parameter(ParameterSetName='Export')][string]$MigratePath = '~\Documents\Migrate_IdList.json',
+  [Parameter(Mandatory, ParameterSetName='Import')][Parameter(Mandatory, ParameterSetName='Export')][Byte[]]$Key,
+  [string]$Encoding = 'UTF8'
+)
+
+# 関数設定
+
+## 文字列暗号化
+function Encryption {
+  param(
+    [string]$String,
+    [byte[]]$Key
+  )
+  $ErrorActionPreference = 'Stop'
+
+  $SecureString = ConvertTo-SecureString -String $String -AsPlainText -Force
+
+  if ( $Key ) {
+    return [string](ConvertFrom-SecureString -SecureString $SecureString -Key $Key)
+  } else {
+    return [string](ConvertFrom-SecureString -SecureString $SecureString)
+  }
+}
+
+## 文字列復号化
+function Decryption {
+  param(
+    [string]$String,
+    [byte[]]$Key
+  )
+  $ErrorActionPreference = 'Stop'
+
+  if ( $Key ) {
+    $SecureString = ConvertTo-SecureString -String $String -Key $Key
+  } else {
+    $SecureString = ConvertTo-SecureString -String $String
+  }
+  return [string]([System.Runtime.InteropServices.Marshal]::PtrToStringBSTR([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureString)))
+}
+
+## ファイル存在確認(フラグON/OFFである場合とない場合のエラー終了に両対応)
+function Test-File {
+  param(
+    [string]$Path,
+    [switch]$IsExist
+  )
+
+  if ( $IsExist -and (Test-Path $Path) ) {
+    Write-Error ('ファイル "{0}" は存在します。' -f $Path) -Category ObjectNotFound
+    exit $false
+  } elseif ( ! $IsExist -and ! (Test-Path $Path) ) {
+    Write-Error ('ファイル "{0}" が見つかりません。' -f $Path) -Category ObjectNotFound
+    exit $false
+  }
+}
+
+# メイン処理
+
+## パスワードファイル読み込み
+$PasswordList = (gc $Path -Encoding $Encoding -ErrorAction SilentlyContinue | ConvertFrom-Json)
+
+## 環境抽出
+$Entry = $PasswordList | ? { $_.Environment -eq $Environment -and $_.Account -eq $Account }
+
+switch ( $PsCmdlet.ParameterSetName ) {
+  ## キー・ユーザー名列挙
+  'List' {
+    Test-File $Path
+    $PasswordList | select Environment,Account
+  }
+
+  ## 復号化した文字列をクリップボードに取得
+  'Get' {
+    Test-File $Path
+    
+    if ( ! $Entry ) { 
+      Write-Error ('環境 "{0} (ユーザ: {1})" の項目は存在しません。' -f $Environment, $Account) -Category InvalidArgument
+      exit $false
+    }
+
+    Write-Host ('環境 "{0} (ユーザ: {1})" のパスワードをクリップボードに取得します。' -f $Entry.Name, $Entry.User)
+    Decryption $Entry.Password | scb
+  }
+
+  ## 暗号化した文字列をファイルに追記 
+  'Set' {
+    if ( $Entry ) {
+      Write-Host ('環境 "{0} (ユーザ: {1})" は既に存在します。' -f $Entry.Name,$Entry.User)
+      if ( (Read-Host '上書きしてもいいですか？(Y/N)') -notmatch "^y(|es)$" ) {
+        exit $true
+      }
+    }
+
+    $PasswordList += [PSCustomObject][Ordered]@{
+      Environment = $Environment
+      Account = $Account
+      Password = Encryption $Password
+    }
+
+    $PasswordList | ConvertTo-Json | sc $Path -Encoding UTF8
+    $PasswordList | select Environment,Account
+  }
+
+  ## 設定した項目を削除
+  'Delete' {
+    Test-File $Path
+
+    if ( ! $Entry ) { 
+      Write-Error ('環境 "{0} (ユーザ: {1})" の項目は存在しません。' -f $Environment, $Account) -Category InvalidArgument
+      exit $false
+    }
+
+    Write-Host ('環境 "{0} (ユーザ: {1})" を削除します。' -f $Entry.Environment, $Entry.Account)
+    if ( (Read-Host '削除してもいいですか？(Y/N)') -notmatch "^y(|es)$" ) {
+      exit $true
+    }
+
+    $PasswordList = $PasswordList | ? { ! ($_.Environment -eq $Entry.Environment -and $_.Account -eq $Entry.Account) }
+    $PasswordList | ConvertTo-Json | sc $Path -Encoding $Encoding
+    $PasswordList | select Environment,Account
+  }
+
+  ## 移行用パスワードファイルを読み込み、端末用パスワードファイルを作成
+  'Import' {
+    Test-File $MigratePath
+    Test-File $Path -IsExist
+
+    (gc $MigratePath -Encoding $Encoding | ConvertFrom-Json) | % {
+      @{
+        Environment = $_.Environment
+        Account = $_.Account
+        Password = Encryption (Decryption $_.Password -Key $Key)
+      }
+    } | ConvertTo-Json | sc $Path -Encoding $Encoding
+
+    ls $Path
+  }
+
+  ## 移行用パスワードファイルを作成
+  'Export' {
+    Test-File $Path
+
+    $PasswordList | % {
+      [Ordered]@{
+        Environment = $_.Environment
+        Account = $_.Account
+        Password = Encryption (Decryption $_.Password) -Key $Key
+      }
+    } | ConvertTo-Json | sc $MigratePath -Encoding $Encoding
+
+    ls $MigratePath
+  }
+}
---
+++ README.md
@@ -0,0 +1,77 @@
+# Manage-Password
+
+平文でパスワード管理したくなかったのと、それをWindowsの標準環境でできるだけ特に何も足さずに実現できる何かが欲しかったので作りました。
+概ね、[Powershellで文字列の暗号化と復号化](https://github.com/senkousya/usingEncryptedStandardStringOnPowershell)を参考にしました。というかそのままです。
+
+## 使用法
+
+オプション | 説明 | 別名 | 省略時の値
+- | - | - | -
+Environment | 環境名 | env, e | 省略不可(Get, Set, Delete指定時のみ)
+Account | アカウント名 | a | 省略不可(Get, Set, Delete指定時のみ)
+Password | パスワード(平文で指定) | p | 省略不可(Set指定時のみ)
+Path | パスワード保存ファイルのパス | - | ~\.idlist
+MigratePath | 移行用パスワードファイルのパス | - | ~\Documents\Migrate_IdList.json
+Key | 移行用パスワードの暗号化キー、byte配列 | - | 省略不可(Import, Export指定時のみ)
+Encoding | パスワードファイルの読み込み、保存に使う文字コード | - | UTF8
+
+### パスワードを追加 
+
+環境名、アカウント名、暗号化されたパスワードを一エントリとして、ファイルに保存します。
+
+```PowerShell
+Manage-Password.ps1 -Set -Environment <string> -Account <string> -Password <string> [-Path <string>] [-Encoding <string>]
+```
+
+### パスワードを取得
+
+環境名、アカウント名を指定して、そのパスワードを取得・復号化し、クリップボードに入れます。
+
+```PowerShell
+Manage-Password.ps1 -Get -Environment <string> -Account <string> [-Path <string>] [-Encoding <string>]
+```
+
+### 保存した情報を削除
+
+環境名、アカウント名を指定して、そのエントリをファイルから削除します。
+
+```PowerShell
+Manage-Password.ps1 -Delete -Environment <string> -Account <string> [-Path <string>] [-Encoding <string>]
+```
+
+### 保存した情報を一覧表示
+
+保存されている環境名とアカウント名を一覧表示します。
+
+```PowerShell
+Manage-Password.ps1 -List
+```
+
+### 移行用ファイルを作成
+
+DPAPIで暗号化されているパスワードファイルから、AESで暗号化されたパスワードファイルを作成します。
+
+```PowerShell
+Manage-Password.ps1 -Export -Key <byte[]> [-Path <string>] [-MigratePath <string>] [-Encoding <string>]
+```
+
+暗号化キーの作成手順例は以下です(via [AESでの暗号化サンプル](https://github.com/senkousya/usingEncryptedStandardStringOnPowershell/blob/master/readme.md#aes%E3%81%A7%E3%81%AE%E6%9A%97%E5%8F%B7%E5%8C%96%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB))。
+
+> ```Powershell
+> #8*24で192bitのバイト配列を作成
+> $EncryptedKey = New-Object Byte[] 24
+> 
+> #RNGCryptoServiceProviderクラスをcreateしてGetBytesメソッドでバイト配列をランダムなデータで埋める。
+> [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($EncryptedKey)
+> 
+> #作成されたランダムな配列を表示
+> $EncryptedKey
+> ```
+
+### 移行用ファイルからパスワードファイルを作成
+
+AESで暗号化されたパスワードファイルから、DPAPIで暗号化されているパスワードファイルを作成します。
+
+```PowerShell
+Manage-Password.ps1 -Import -Key <byte[]> [-Path <string>] [-MigratePath <string>] [-Encoding <string>]
+```
